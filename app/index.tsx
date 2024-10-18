3adf3eb3b35516fe4ddaf67d0e044cf133776b02
Victor Chagas <victorchagaslm@gmail.com>
2024-10-18 09:40:56 -0300

    Desafio 1 flexbox

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,85 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View
+        style={{
+          borderStyle: "solid",
+          borderColor: "#403F47",
+          borderWidth: 6,
+        }}
+      >
+        <View style={{ flexDirection: "row", height: 70, width: "70%" }}>
+          <View
+            style={{
+              flex: 2.5,
+              borderStyle: "solid",
+              borderColor: "#403F47",
+              borderWidth: 3,
+              backgroundColor: "#5271FF",
+            }}
+          />
+          <View
+            style={{
+              flex: 1,
+              borderStyle: "solid",
+              borderColor: "#403F47",
+              borderWidth: 3,
+              backgroundColor: "#38B6FF",
+            }}
+          />
+          <View
+            style={{
+              flex: 1,
+              borderStyle: "solid",
+              borderColor: "#403F47",
+              borderWidth: 3,
+              backgroundColor: "#8F6BE5",
+            }}
+          />
+        </View>
+        <View style={{ flexDirection: "row", height: 70, width: "70%" }}>
+          <View
+            style={{
+              flex: 1,
+              borderStyle: "solid",
+              borderColor: "#403F47",
+              borderWidth: 3,
+              backgroundColor: "#612CC5",
+            }}
+          />
+          <View
+            style={{
+              flex: 2.5,
+              justifyContent: "center",
+              alignItems: "center",
+              borderStyle: "solid",
+              borderColor: "#403F47",
+              borderWidth: 3,
+              backgroundColor: "#38B6FF",
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "monospace",
+                fontWeight: "bold",
+                fontSize: 30,
+                letterSpacing: 4,
+              }}
+            >
+              FLEXBOX
+            </Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              borderStyle: "solid",
+              borderColor: "#403F47",
+              borderWidth: 3,
+              backgroundColor: "#93C7FF",
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
